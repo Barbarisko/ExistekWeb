0d3a26185ef1e2d376f8708dfa168fe53844c97d
Barbarisko <helen.fedchuk@gmail.com>
2021-08-11 07:40:08 +0300

    branch for new config files

--- ExistekWEbProject/PublishOptions.cs
+++ ExistekWEbProject/PublishOptions.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 namespace ExistekWEbProject
 {
+    //this publish options will be applied, if no article is created
+    //Configurations will be inserted here
     public class PublishOptions
     {
         public string filename;
