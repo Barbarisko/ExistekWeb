b6826a5f5bac0565c0cb1a3c9802bfa294f8ce00
Barbarisko <helen.fedchuk@gmail.com>
2021-08-18 11:31:46 +0300

    initial branch commit

---
+++ ExistekWEbProject/CustomFilters/ExistingArticleExceptionFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExistekWEbProject.CustomFilters
+{
+//    - Cтворити фільтр, який буде перехоплювати якийсь конкретний Exception(бажано створити власний)
+//Фільтр повинен логувати цей Exception і повертати клієнту щось типу "Цей запит не може бути оброблено".
+    public class ExistingArticleExceptionFilter
+    {
+    }
+}
